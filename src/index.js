5242907b24b30981656aeb38d42332da61e0b97b
taldr27 <diegogarciaq98@gmail.com>
2022-11-21 16:54:13 -0500

    Move provider from app to index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/configureStore';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
