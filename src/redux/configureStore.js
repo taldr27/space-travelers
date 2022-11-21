d9341534038da2a5613fa75b2c0df0cf7abea11c
Jhosep <jose-zromero@hotmail.com>
2022-11-21 16:34:02 -0600

    Apply linters

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
- import missionsReducer from './missions/Missions';
- import rocketsReducer from './rockets/Rockets';
+import missionsReducer from './missions/Missions';
+import rocketsReducer from './rockets/Rockets';
 
 const store = configureStore({
   reducer: {
-     missions: missionsReducer,
-     rockets: rocketsReducer,
+    missions: missionsReducer,
+    rockets: rocketsReducer,
   },
 });
 export default store;
