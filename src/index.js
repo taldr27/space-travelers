efaa55a333f0e8878bc7620c78d289deb30c5b57
taldr27 <diegogarciaq98@gmail.com>
2022-11-21 12:46:26 -0500

    Add folders and store for redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/configureStore';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
